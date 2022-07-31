dc36a1737c6605520adc045ead0e9f28546c5897
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-07-31 19:51:34 +0500

    fix:fixed some problems

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
 // You should implement your task here.
 
 module.exports =     function towelSort(arr){
+      if ((towelSort.arguments.length == 0)) {
+    return [];}
+    else{
         let result=[]; 
         let k=0; 
         for( let i=0;i<arr.length;i++){
@@ -13,5 +16,5 @@ module.exports =     function towelSort(arr){
             else{
             arr[i]=arr[i].sort(function(a,b){return a-b})
             for(let j=0;j<arr[i].length;j++){
-                result[k]=arr[i][j]; k++ }}}
+                result[k]=arr[i][j]; k++ }}}}
         return result}
