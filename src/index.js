b87abae06c36db8153707f05db3827be38622459
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-07-31 19:05:40 +0500

    feat: function added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    function towelSort(arr){
+        let result=[]; 
+        let k=0; 
+        for( let i=0;i<arr.length;i++){
+            for(let j=0;j<arr[i].length;j++){
+                result[k]=arr[i][j]; k++ }} 
+        return result}
 }
