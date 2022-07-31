fb0aea34f6eb0f588b2c6954cd778406a3451933
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-07-31 19:45:44 +0500

    fix: towel soft implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-    function towelSort(arr){
+module.exports =     function towelSort(arr){
         let result=[]; 
         let k=0; 
         for( let i=0;i<arr.length;i++){
+            if(i%2!==0){
+                arr[i]=arr[i].sort(function(a,b){return a-b}).reverse()
+                for(let j=0;j<arr[i].length;j++){
+                result[k]=arr[i][j]; k++ } 
+            }
+            else{
+            arr[i]=arr[i].sort(function(a,b){return a-b})
             for(let j=0;j<arr[i].length;j++){
-                result[k]=arr[i][j]; k++ }} 
+                result[k]=arr[i][j]; k++ }}}
         return result}
-}
